f694de2ebf8df97b3b5515f370ad7736f58ac5c7
The Krys <krysthopher@gmail.com>
2024-06-16 14:55:24 -0600

    adding movie app

--- More React Hooks/movie-app/src/App.jsx
+++ More React Hooks/movie-app/src/App.jsx
@@ -1,19 +1,15 @@
-import React from "react"
-import { MoviesGrid } from "./components/MoviesGrid"
-import styles from './App.module.css'
+import { MoviesGrid } from "./components/MoviesGrid";
+import styles from "./App.module.css";
 
 export function App() {
-
   return (
     <div>
       <header>
         <h1 className={styles.title}>Movies</h1>
       </header>
-
       <main>
         <MoviesGrid />
       </main>
-
     </div>
-  )
+  );
 }
